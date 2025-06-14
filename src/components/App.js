2366a506e6550198a3b7b9793494cf6b449b8e4d
Eliud Metto <eliudmett093@gmail.com>
2025-06-14 09:58:36 +0300

    Added new content to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
+import "./App.css";
+
 const App = () => {
+	useEffect(() => {
+		const handleBodyScroll = () => {
+			console.log("body scrolled");
+		};
+
+		window.addEventListener("scroll", handleBodyScroll);
+
+		return () => window.removeEventListener("scroll", handleBodyScroll);
+	});
+
 	return <h1>App</h1>;
 };
 
