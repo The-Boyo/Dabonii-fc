9286f1949a74110c0ce86c4d4eab7db7203d13e2
Eliud Metto <eliudmett093@gmail.com>
2025-06-16 22:57:48 +0300

    Added focus to the nav elements to show the clicked element when its page is being viewed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
+import { useEffect, useRef } from "react";
 
 const Header = () => {
+	const headerRef = useRef();
+
+	const location = useLocation();
+
+	useEffect(() => {
+		const handleBodyScroll = () => {
+			console.log("body has scrolled...");
+			console.log(window.scrollY);
+
+			if (window.scrollY > 100) {
+				headerRef.current.style.position = "fixed";
+			} else {
+				headerRef.current.style.position = "relative";
+			}
+		};
+
+		window.addEventListener("scroll", handleBodyScroll);
+
+		return () => window.removeEventListener("scroll", handleBodyScroll);
+	});
+
 	return (
-		<header className="header">
+		<header className="header" ref={headerRef}>
 			<h2 className="header_logo">
 				<Link to={"/"} className="header_logo-link">
 					fc <span>Dabonii</span>
@@ -11,22 +33,46 @@ const Header = () => {
 			</h2>
 			<div className="nav-bar">
 				<Link to={"/office"}>
-					<h3 className="nav_elem nav_office">Office</h3>
+					<h3
+						className={`nav_elem nav_office ${location.pathname.includes("office") ? "active_nav-bar" : ""}`}
+					>
+						Office
+					</h3>
 				</Link>
 				<Link to={"/players"}>
-					<h3 className="nav_elem nav_players">Players</h3>
+					<h3
+						className={`nav_elem nav_players ${location.pathname.includes("players") ? "active_nav-bar" : ""} `}
+					>
+						Players
+					</h3>
 				</Link>
 				<Link to={"/fans"}>
-					<h3 className="nav_elem nav_fans">Fans</h3>
+					<h3
+						className={`nav_elem nav_fans ${location.pathname.includes("fans") ? "active_nav-bar" : ""}`}
+					>
+						Fans
+					</h3>
 				</Link>
 				<Link to={"/events"}>
-					<h3 className="nav_elem nav_events">Events</h3>
+					<h3
+						className={`nav_elem nav_events ${location.pathname.includes("events") ? "active_nav-bar" : ""}`}
+					>
+						Events
+					</h3>
 				</Link>
 				<Link to={"/matches"}>
-					<h3 className="nav_elem nav_matches">Matches</h3>
+					<h3
+						className={`nav_elem nav_matches ${location.pathname.includes("matches") ? "active_nav-bar" : ""}`}
+					>
+						Matches
+					</h3>
 				</Link>
 				<Link to={"/training"}>
-					<h3 className="nav_elem nav_training">Training</h3>
+					<h3
+						className={`nav_elem nav_training ${location.pathname.includes("training") ? "active_nav-bar" : ""}`}
+					>
+						Training
+					</h3>
 				</Link>
 			</div>
 			<div className="auth">
