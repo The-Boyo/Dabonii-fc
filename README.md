644a8fb0e23686f11bfc2178473722418371e78f
Eliud Metto <eliudmett093@gmail.com>
2025-08-03 10:24:15 +0300

    Added a modal to display a player's full information

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Player as an emoji
+
+An emoji to describe the personality of a player
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import store from "./store/store";
 
 import App from "./components/App";
 import { Provider } from "react-redux";
+import { createPortal } from "react-dom";
+import PlayerDetailsModal from "./components/modal/PlayerDetailsModal";
 
 const root = createRoot(document.getElementById("root"));
 
-console.log(store);
+export const portal = createPortal(
+	<PlayerDetailsModal />,
+	document.getElementById("modal")
+);
 
 root.render(
 	<StrictMode>
