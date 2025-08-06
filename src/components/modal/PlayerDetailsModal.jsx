ce2f7a59f652d0184c0862c43443fb82b4a0376e
Eliud Metto <eliudmett093@gmail.com>
2025-08-06 11:49:01 +0300

    added player data to the modal that appears after you click on a player profile

--- src/components/modal/PlayerDetailsModal.jsx
+++ src/components/modal/PlayerDetailsModal.jsx
@@ -13,16 +13,62 @@ const PlayerDetailsModal = ({ onClose, playerName }) => {
 
 	return (
 		<div className="modal-cont">
-			<div className="theModal">
+			<div className="the-modal">
 				<h2>Player Information</h2>
+				<img src={getImages()} alt="" />
 				<div className="player-data">
 					<div className="player-data-personal">
-						<img src={getImages()} alt="" />
-						<h3>{theRenderedPlayer.name}</h3>
+						<p>
+							Name: <span>{theRenderedPlayer.name}</span>
+						</p>
+						<p>
+							Alias: <span>Kento</span>
+						</p>
+						<p>
+							Position: <span>Attacking Midfielder</span>
+						</p>
+						<p>
+							Height: <span>182cm</span>
+						</p>
+						<p>
+							Strong Foot: <span>Left</span>
+						</p>
+						<p className="player-emoji">
+							Player as Emoji: <span className="player-as-emoji">😁</span>{" "}
+						</p>
 					</div>
-					<div className="player-data-games"></div>
+					<div className="player-data-games">
+						<p>
+							Joined: <span>2016</span>
+						</p>
+						<p>
+							Games Played: <span>30</span>
+						</p>
+						<p>
+							Goals: <span>10</span>
+						</p>
+						<p>
+							Assists <span>5</span>
+						</p>
+					</div>
+					<div className="player-attributes">
+						<p className="player-strengths">
+							Strengths: <span>{theRenderedPlayer.strengths}</span>
+						</p>
+						<p>
+							Favourite Move: <span>The Chop</span>
+						</p>
+						<div className="player-insights">
+							<h4>Player's Insights</h4>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Praesentium numquam eius autem quo corporis architecto minus,
+								error veniam voluptates fugit at provident itaque doloribus
+								illum commodi repellat suscipit. Dolorum, natus.
+							</p>
+						</div>
+					</div>
 				</div>
-				<div className="player-attributes"></div>
 				<button className="btn-close" onClick={onClose}>
 					Close
 				</button>
